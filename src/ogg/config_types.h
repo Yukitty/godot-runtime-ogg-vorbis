6c564e78742ac5e4c88f77443f8ff087ee1a43f3
Yukita Mayako <catgirl@goddess.moe>
2023-11-28 16:40:25 -0500

    Add ogg/config_types.h to project
    I don't know enough about SCons to make it do what CMake does here.

---
+++ src/ogg/config_types.h
@@ -0,0 +1,15 @@
+// Dirty pre-configure for libogg:
+// We're using the C17 standard, so stdint should be available on all platforms.
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+
+#include <stdint.h>
+
+typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
+
+#endif
